be5e0615f900d5895d7f39839e74ccd5d4d5b1f1
unknown <jason.rettinger@gmail.com>
2022-08-22 23:19:42 -0400

    rough draft css & added one question in js. getting tired

---
+++ .assets/js/script.js
@@ -0,0 +1,8 @@
+// Questions & Answers
+var questions = [
+    {
+        title: "Which of the following methods can be used to display data in some form using Javascript? ",
+        options: ["document.write()", "console.log()", "window.alert()", "All of the above"],
+        answer: ["All of the above"]
+    },
+]
