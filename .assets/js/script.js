900e6d5c2a9a6f312de65d82e4066c257dfbcce2
unknown <jason.rettinger@gmail.com>
2022-08-23 09:34:25 -0400

    added three more Q&A

--- .assets/js/script.js
+++ .assets/js/script.js
@@ -1,9 +1,9 @@
 // Questions & Answers
 var questions = [
     {
-        title: "Which of the following methods can be used to display data in some form using Javascript? ",
+        title: "Which of the following methods can be used to display data in some form using Javascript?",
         options: ["document.write()", "console.log()", "window.alert()", "All of the above"],
-        answer: ["All of the above"]
+        answer: "All of the above"
     },
 
     {
@@ -11,4 +11,28 @@ var questions = [
     options: ["const", "var", "let", "constant"],
     answer: "const"
     },
+
+    {
+    title: "JavaScript is a(n) _______ language?",
+    options: ["Object-Oriented", "Object-Based", "Procedural", "Foreign"],
+    answer: "Object-Oriented"
+    },
+
+    {
+        title: "Which built-in method returns the calling string value converted to lower case?",
+        options: ["toLowercase()", "toLower()", "Lowercase()", "None of the above"],
+        answer: "toLowercase()"
+    },
+   
+    {
+    title: "Commonly used data types do not inlcude:",
+    options: ["booleans", "alerts", "strings", "numbers"],
+    answer: "alerts"
+    },
+
+
+
+
+
+
 ]
